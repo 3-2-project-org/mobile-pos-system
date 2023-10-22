018f8dbddaa0f91a3abef66e3a947731b313871f
thushanvithana <87628351+thushanvithana@users.noreply.github.com>
2023-10-22 22:01:11 +0530

    add stock stock  done!

--- src/screens/Inventory/InventorySummaryScreen.jsx
+++ src/screens/Inventory/InventorySummaryScreen.jsx
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  ActivityIndicator,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
 import { axiosInstance } from "../../utils/common/api";
+import { BASIC_COLORS } from "../../utils/constants/styles";
 
 const InventorySummaryScreen = () => {
   const [loading, setLoading] = useState(true);
@@ -12,7 +19,6 @@ const InventorySummaryScreen = () => {
       .then((response) => {
         console.log("Data received:", response.data.data.data);
         setProducts(response.data.data.data);
-
         setLoading(false);
       })
       .catch((error) => {
@@ -22,31 +28,84 @@ const InventorySummaryScreen = () => {
   }, []);
 
   return (
-    <View style={{ flex: 1, padding: 16 }}>
-      <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 16 }}>
-        Inventory Summary
-      </Text>
-      {loading ? (
-        <ActivityIndicator size="large" color="#0000ff" />
-      ) : (
-        <View>
-          {products &&
-            products.length > 0 &&
-            products.map((item) => (
-              <View key={item._id} style={{ marginBottom: 16 }}>
-                <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-                  {item.name}
-                </Text>
-                <Text>{item.description}</Text>
-                <Text>Price: ${item.price}</Text>
-                <Text>Total Stock: {item.totalStock}</Text>
-                <Text>In Stock: {item.inStock}</Text>
-              </View>
-            ))}
-        </View>
-      )}
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 16 }}>
+          Inventory Summary
+        </Text>
+        {loading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <View>
+            {products &&
+              products.length > 0 &&
+              products.map((item) => (
+                <View key={item._id} style={styles.card}>
+                  <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+                    {item.name}
+                  </Text>
+                  <Text>{item.description}</Text>
+                  <View style={styles.row}>
+                    <View style={styles.column}>
+                      <Text style={styles.labelText}>Price:</Text>
+                    </View>
+                    <View style={styles.column}>
+                      <Text style={styles.valueText}>Rs {item.price}</Text>
+                    </View>
+                  </View>
+                  <View style={styles.row}>
+                    <View style={styles.column}>
+                      <Text style={styles.labelText}>Total Stock:</Text>
+                    </View>
+                    <View style={styles.column}>
+                      <Text style={styles.valueText}>{item.totalStock}</Text>
+                    </View>
+                  </View>
+                  <View style={styles.row}>
+                    <View style={styles.column}>
+                      <Text style={styles.labelText}>In Stock:</Text>
+                    </View>
+                    <View style={styles.column}>
+                      <Text style={styles.valueText}>{item.inStock}</Text>
+                    </View>
+                  </View>
+                </View>
+              ))}
+          </View>
+        )}
+      </View>
+    </ScrollView>
   );
 };
 
 export default InventorySummaryScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  card: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 8,
+    marginTop: 13,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  column: {
+    flex: 1,
+  },
+  labelText: {
+    fontSize: 14,
+    fontWeight: "500",
+    textAlign: "left",
+  },
+  valueText: {
+    color: BASIC_COLORS.FONT_SECONDARY, // You need to define BASIC_COLORS.
+    textAlign: "left",
+  },
+});

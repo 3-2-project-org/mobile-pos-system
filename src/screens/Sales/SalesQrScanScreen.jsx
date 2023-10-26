02b3cf2dd79efcca7118b990cbd00935b5d2ea22
thushanvithana <87628351+thushanvithana@users.noreply.github.com>
2023-10-26 13:45:29 +0530

    qr fixed

--- src/screens/Sales/SalesQrScanScreen.jsx
+++ src/screens/Sales/SalesQrScanScreen.jsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+} from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import { MaterialCommunityIcons as Icon } from "@expo/vector-icons";
 import { BASIC_COLORS } from "../../utils/constants/styles";
@@ -9,9 +16,21 @@ import { useNavigation } from "@react-navigation/native";
 
 const SalesQrScanScreen = () => {
   const navigation = useNavigation();
+  const [showPopup, setShowPopup] = useState(false);
+  const [selectedData, setSelectedData] = useState("");
+
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
+  const [flash, setFlash] = useState(false);
+  const [scannedData, setScannedData] = useState([]);
 
+  const togglePopup = (data) => {
+    setSelectedData(data);
+    setShowPopup(!showPopup);
+  };
+  useEffect(() => {
+    setScanned(false);
+  }, [navigation]);
   useEffect(() => {
     const getBarCodeScannerPermissions = async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
@@ -23,32 +42,86 @@ const SalesQrScanScreen = () => {
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
+    setScannedData((prevData) => [...prevData, data]);
+
     navigation.navigate("ScannedDataDisplay", { scannedData: [data] });
+  };
+
+  const toggleFlash = () => {
+    setFlash(!flash);
   };
 
   if (hasPermission === null) {
     return <Text>Requesting for camera permission</Text>;
   }
   if (hasPermission === false) {
-    return <Text>No access to the camera</Text>;
+    return <Text>No access to camera</Text>;
   }
 
   return (
     <View style={styles.container}>
       <BarCodeScanner
-        onBarCodeScanned={handleBarCodeScanned}  // Remove the condition here
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
+        torchMode={flash ? "on" : "off"}
       />
+      <View style={styles.overlay}>
+        <View style={styles.scanArea} />
+      </View>
+
+      <TouchableOpacity
+        onPress={() => togglePopup(scannedData)}
+        style={[styles.button, { position: "absolute", top: 20 }]} // Adjust the button style
+      >
+        <Text style={styles.buttonText}>Show Scanned Data</Text>
+      </TouchableOpacity>
 
       <View style={styles.buttonsContainer}>
-        <MPSButton
-          icon={<QrIcon />}
-          buttonType={"primary"}
-          onPress={() => setScanned(false)} // Reset the scanned state to allow scanning again
-          buttonTitle={"Tap to Scan Again"}
-          buttonStyle={{ height: 67 }}
-        />
+        {scanned ? (
+          <MPSButton
+            icon={<QrIcon />}
+            buttonType={"primary"}
+            onPress={() => setScanned(false)}
+            buttonTitle={"Tap to Scan Again"}
+            buttonStyle={{ height: 67 }}
+          />
+        ) : (
+          <MPSButton
+            icon={<QrIcon />}
+            buttonType={"primary"}
+            onPress={() => setScanned(true)}
+            buttonTitle={"Scan QR Code"}
+            buttonStyle={{ height: 67 }}
+          />
+        )}
       </View>
+
+      <Modal
+        visible={showPopup}
+        animationType="slide"
+        onRequestClose={() => setShowPopup(false)}
+      >
+        <View style={{ marginTop: 30 }}>
+          <TouchableOpacity
+            onPress={() => setShowPopup(false)}
+            style={styles.closeButton}
+          >
+            <Icon name="close" size={35} color={BASIC_COLORS.PRIMARY} />
+          </TouchableOpacity>
+
+          <View style={{ marginTop: 30, paddingLeft: 30 }}>
+            <FlatList
+              data={selectedData}
+              renderItem={({ item }) => (
+                <View style={styles.popupItem}>
+                  <Text style={styles.popupItemText}>QR code: {item}</Text>
+                </View>
+              )}
+              keyExtractor={(item, index) => index.toString()}
+            />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
